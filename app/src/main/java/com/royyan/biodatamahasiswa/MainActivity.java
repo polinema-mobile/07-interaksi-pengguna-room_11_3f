f67431f5d3e6ba4f33600e41167305b92efec916
muhammadroyyan11 <royyanmz87@gmail.com>
2020-09-29 09:39:51 +0700

    Layout selesai

--- app/src/main/java/com/royyan/biodatamahasiswa/MainActivity.java
+++ app/src/main/java/com/royyan/biodatamahasiswa/MainActivity.java
@@ -3,12 +3,38 @@ package com.royyan.biodatamahasiswa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Parcelable {
+
+    protected MainActivity(Parcel in) {
+    }
+
+    public static final Creator<MainActivity> CREATOR = new Creator<MainActivity>() {
+        @Override
+        public MainActivity createFromParcel(Parcel in) {
+            return new MainActivity(in);
+        }
+
+        @Override
+        public MainActivity[] newArray(int size) {
+            return new MainActivity[size];
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
     }
 }
